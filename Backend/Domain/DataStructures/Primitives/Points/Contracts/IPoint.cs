c19acea060e1f2341cab74fbf4b67d9253d0bc75
JankowskiKamil <mr.kamil.jankowski@gmail.com>
2023-03-25 23:18:10 +0100

    feat(Backend, Points): IPoint interface added

---
+++ Backend/Domain/DataStructures/Primitives/Points/Contracts/IPoint.cs
@@ -0,0 +1,10 @@
+﻿using System.Numerics;
+
+namespace Backend.Domain.DataStructures.Primitives.Points.Contracts;
+
+public interface IPoint<T> where T : INumber<T>
+{
+    public T X { get; }
+
+    public T Y { get; }
+}
